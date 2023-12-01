4645cb618f0272678dcbf7a9be018b7d40145ea9
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-12-01 19:28:19 +0300

    Create and Retrieve with Flask-RESTful

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, make_response
+from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+
 from models import db, Newsletter
 
 app = Flask(__name__)
@@ -15,23 +16,38 @@ db.init_app(app)
 
 api = Api(app)
 
-class Home(Resource):
+class Index(Resource):
+
     def get(self):
+
         response_dict = {
-            "message": "Welcome to the Newsletter RESTful API",
+            "index": "Welcome to the Newsletter RESTful API",
         }
-        response = make_response(response_dict, 200)
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
         return response
 
-api.add_resource(Home, '/')
+api.add_resource(Index, '/')
 
 class Newsletters(Resource):
+
     def get(self):
+
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
-        response = make_response(response_dict_list, 200)
+
+        response = make_response(
+            jsonify(response_dict_list),
+            200,
+        )
+
         return response
 
     def post(self):
+
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
@@ -43,7 +59,7 @@ class Newsletters(Resource):
         response_dict = new_record.to_dict()
 
         response = make_response(
-            response_dict,
+            jsonify(response_dict),
             201,
         )
 
@@ -52,12 +68,20 @@ class Newsletters(Resource):
 api.add_resource(Newsletters, '/newsletters')
 
 class NewsletterByID(Resource):
+
     def get(self, id):
+
         response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
-        response = make_response(response_dict, 200)
+
+        response = make_response(
+            jsonify(response_dict),
+            200,
+        )
+
         return response
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
